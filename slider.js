d1712b69449996b99c4c0e66e7a072c22f212478
Sharayu Rajput <sharayurajput1998@gmail.com>
2026-03-28 16:14:16 +0530

    merge remote changes

--- slider.js
+++ slider.js
@@ -1,35 +1,42 @@
 const slides = document.querySelector('.slides');
-let imgs = document.querySelectorAll('.slide');
 const prev_btn = document.querySelector('.prev');
 const next_btn = document.querySelector('.next');
+let imgs = document.querySelectorAll('.slide');
 
+// clone first image
 const firstClone = imgs[0].cloneNode(true);
 slides.appendChild(firstClone);
 
+// clone last image
 const lastClone = imgs[imgs.length-1].cloneNode(true);
 slides.insertBefore(lastClone, slides.firstChild);
 
+// update imgs
 imgs = document.querySelectorAll('.slide');
 
-n = 1;
+//  NO animation at start
+slides.style.transition = 'none';
+
+let n = 1;
 
 function changeSlide() {
-    slides.style.transform = `translateX(-${n * 100}%)`;
+    slides.style.transform = `translateX(-${n*100}%)`;
 }
+changeSlide();
 
 prev_btn.addEventListener('click', () => {
     n--;
-    slides.style.transition = 'transform 0.5s ease';
-
+    slides.style.transition = "transform 0.5s ease";
     changeSlide();
 });
 
 next_btn.addEventListener('click', () => {
     n++;
-    slides.style.transition = 'transform 0.5s ease';
+    slides.style.transition = "transform 0.5s ease";
     changeSlide();
 });
 
+// infinite loop fix (both sides)
 slides.addEventListener('transitionend', () => {
     if (n === imgs.length-1) {
         slides.style.transition = 'none';
@@ -43,3 +50,10 @@ slides.addEventListener('transitionend', () => {
         changeSlide();
     }
 });
+
+// auto slide
+setInterval(() => {
+    n++;
+    slides.style.transition = "transform 0.5s ease";
+    changeSlide();
+}, 5000);
